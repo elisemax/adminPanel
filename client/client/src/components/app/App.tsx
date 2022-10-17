c7e18a1a07b572fd102a95d46253ae5577fd912d
Max <max11cz@seznam.cz>
2022-10-17 13:29:21 +0200

    add

--- client/client/src/components/app/App.tsx
+++ client/client/src/components/app/App.tsx
@@ -3,7 +3,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <h1 className="text-3xl font-bold underline text-red-600">
+          Simple React Typescript Tailwind Sample
+        </h1>
       </header>
     </div>
   );
